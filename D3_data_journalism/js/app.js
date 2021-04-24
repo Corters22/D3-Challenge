9f4c0ffb9f9c906f40c0c977ab86858a82810a06
code_red <72528267+Corters22@users.noreply.github.com>
2021-04-24 08:41:45 -0500

    finished code, haven't checked index.html

--- D3_data_journalism/js/app.js
+++ D3_data_journalism/js/app.js
@@ -240,93 +240,176 @@ d3.csv("data/data.csv").then(function(fullData) {
 
       // functions here found above csv import
       // updates x scale for new data
-      xLinearScale = xScale(hairData, chosenXAxis);
+      xLinearScale = xScale(fullData, chosenXAxis);
 
       // updates x axis with transition
-      xAxis = renderAxes(xLinearScale, xAxis);
+      xAxis = renderXAxes(xLinearScale, xAxis);
+
+      //current y-axis
+      yLinearScale = yScale(fullData, chosenYAxis);
+      yAxis = renderYAxes(yLinearScale, yAxis);
 
       // updates circles with new x values
-      circlesGroup = renderCircles(circlesGroup, xLinearScale, chosenXAxis);
+      circlesGroup = renderCircles(circlesGroup, xLinearScale, chosenXAxis, yLinearScale, chosenYAxis);
 
       // updates tooltips with new info
-      circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
+      circlesGroup = updateToolTip(chosenXAxis, chosenYAxis, circlesGroup);
 
           // changes classes to change bold text
-          if (chosenXAxis === "num_albums") {
-            albumsLabel
+          if (chosenXAxis === "poverty") {
+            povertyLabel
               .classed("active", true)
               .classed("inactive", false);
-            hairLengthLabel
+            ageLabel
               .classed("active", false)
               .classed("inactive", true);
+            incomeLabel
+                .classed('active', false)
+                .classed('inactive', true)
+          }
+          else if(chosenXAxis === 'age') {
+            povertyLabel
+              .classed("active", false)
+              .classed("inactive", true);
+            ageLabel
+              .classed("active", true)
+              .classed("inactive", false);
+            incomeLabel 
+                .classed('active', false)
+                .classed('inactive', true)
           }
           else {
-            albumsLabel
-              .classed("active", false)
-              .classed("inactive", true);
-            hairLengthLabel
-              .classed("active", true)
-              .classed("inactive", false);
+              povertyLabel
+                .classed('active', false)
+                .classed('inactive', true)
+            ageLabel    
+                .classed('active', false)
+                .classed('inactive', true)
+            incomeLabel 
+                .classed('active', true)
+                .classed('inactive', false)
           }
         }
       });
+
+        // x axis labels event listener
+    ylabelsGroup.selectAll("text")
+        .on("click", function() {
+    // get value of selection
+    var value = d3.select(this).attr("value");
+    if (value !== chosenYAxis) {
+
+    // replaces chosenYAxis with value
+    chosenYAxis = value;
+
+    // console.log(chosenYAxis)
+
+  // functions here found above csv import
+  // updates y scale for new data
+  yLinearScale = yScale(fullData, chosenYAxis);
+
+  // updates x axis with transition
+  yAxis = renderYAxes(yLinearScale, yAxis);
+
+  // updates circles with new x values
+  circlesGroup = renderCircles(circlesGroup, xLinearScale, chosenXAxis, yLinearScale, chosenYAxis);
+
+  // updates tooltips with new info
+  circlesGroup = updateToolTip(chosenXAxis, chosenYAxis, circlesGroup);
+
+      // changes classes to change bold text
+      if (chosenYAxis === "healthcare") {
+        healthcareLabel
+          .classed("active", true)
+          .classed("inactive", false);
+        smokeLabel
+          .classed("active", false)
+          .classed("inactive", true);
+        obeseLabel
+            .classed('active', false)
+            .classed('inactive', true)
+      }
+      else if(chosenYAxis === 'smoke') {
+        healthcareLabel
+          .classed("active", false)
+          .classed("inactive", true);
+        smokeLabel
+          .classed("active", true)
+          .classed("inactive", false);
+        obeseLabel 
+            .classed('active', false)
+            .classed('inactive', true)
+      }
+      else {
+          healthcareLabel
+            .classed('active', false)
+            .classed('inactive', true)
+        smokeLabel    
+            .classed('active', false)
+            .classed('inactive', true)
+        obeseLabel 
+            .classed('active', true)
+            .classed('inactive', false)
+      }
+    }
+  });
     
-        // Create initial axis functions
-    var bottomAxis = d3.axisBottom(xLinearScale);
-    var leftAxis = d3.axisLeft(yLinearScale);
+//         // Create initial axis functions
+//     var bottomAxis = d3.axisBottom(xLinearScale);
+//     var leftAxis = d3.axisLeft(yLinearScale);
     
-    //create x scale for age
-    var xAgeScale = d3.scaleLinear()
-        .domain([30, d3.max(fullData, d => d.age)])
-        .range([0, chartWidth]);
+//     //create x scale for age
+//     var xAgeScale = d3.scaleLinear()
+//         .domain([30, d3.max(fullData, d => d.age)])
+//         .range([0, chartWidth]);
         
-    // create y scale for smokers
-    var ySmokeScale = d3.scaleLinear()
-        .domain([0, d3.max(fullData, d => d.smokes)])
-        .range([chartHeight, 0])
+//     // create y scale for smokers
+//     var ySmokeScale = d3.scaleLinear()
+//         .domain([0, d3.max(fullData, d => d.smokes)])
+//         .range([chartHeight, 0])
     
 
-      // Create two new functions passing the scales in as arguments
-    // These will be used to create the chart's axes
-    var bottomAxis = d3.axisBottom(xAgeScale);
-    var leftAxis = d3.axisLeft(ySmokeScale);
+//       // Create two new functions passing the scales in as arguments
+//     // These will be used to create the chart's axes
+//     var bottomAxis = d3.axisBottom(xAgeScale);
+//     var leftAxis = d3.axisLeft(ySmokeScale);
 
-      // Append an SVG group element to the SVG area, create the left axis inside of it, and give it a class of "axis"
-    chartGroup.append('g')
-      .classed('axis', true)
-      .call(leftAxis);
-   // Append an SVG grou p element to the SVG area, create the bottom axis inside of it
-    // Translate the bottom axis to the bottom of the page
-    // Give it a class of "axis"
-    chartGroup.append('g')
-        .classed('axis', true)
-        .attr("transform", `translate(0, ${chartHeight})`)
-        .call(bottomAxis);
+//       // Append an SVG group element to the SVG area, create the left axis inside of it, and give it a class of "axis"
+//     chartGroup.append('g')
+//       .classed('axis', true)
+//       .call(leftAxis);
+//    // Append an SVG grou p element to the SVG area, create the bottom axis inside of it
+//     // Translate the bottom axis to the bottom of the page
+//     // Give it a class of "axis"
+//     chartGroup.append('g')
+//         .classed('axis', true)
+//         .attr("transform", `translate(0, ${chartHeight})`)
+//         .call(bottomAxis);
 
     
-    // create function to draw circles at coordinates
-    // Configure a line function which will plot the x and y coordinates using our scales
-    // var drawLine = d3.line()
-    //     .x(d => xAgeScale(d.age))
-    //     .y(d => ySmokeScale(d.smokes));
+//     // create function to draw circles at coordinates
+//     // Configure a line function which will plot the x and y coordinates using our scales
+//     // var drawLine = d3.line()
+//     //     .x(d => xAgeScale(d.age))
+//     //     .y(d => ySmokeScale(d.smokes));
 
-    // chartGroup.append('path')
-    //     .data([fullData])
-    //     .attr('d', drawLine)
-    //     .attr("fill", "none")
-    //     .classed('line', true)
-    //     .attr("stroke", "red");
+//     // chartGroup.append('path')
+//     //     .data([fullData])
+//     //     .attr('d', drawLine)
+//     //     .attr("fill", "none")
+//     //     .classed('line', true)
+//     //     .attr("stroke", "red");
  
-    var circles = chartGroup.selectAll('circle');
+//     var circles = chartGroup.selectAll('circle');
 
-    circles.data(fullData)
-        .enter()
-        .append('circle')
-        .attr('cx', d => xAgeScale(d.age))
-        .attr('cy', d => ySmokeScale(d.smokes))
-        .attr('r', 10)
-        .attr('stroke', 'black')
-        .attr('fill', 'lightblue')
+//     circles.data(fullData)
+//         .enter()
+//         .append('circle')
+//         .attr('cx', d => xAgeScale(d.age))
+//         .attr('cy', d => ySmokeScale(d.smokes))
+//         .attr('r', 10)
+//         .attr('stroke', 'black')
+//         .attr('fill', 'lightblue')
     
     // console.log(bottomAxis);
 })
